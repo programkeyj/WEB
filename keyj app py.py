d8c4c588b672e95df2f2935e12ef4e3b7f659ba2
programkeyj <74073784+programkeyj@users.noreply.github.com>
2020-11-06 22:00:32 +0200

    Add files via upload

---
+++ keyj app py.py
@@ -0,0 +1,106 @@
+from tkinter import*
+
+
+def bA_click():
+     label.config(text = 'password вернитесь в консоль')
+def bB_click():
+     label.config(text = 'ваш код 5467 вернитесь в консоль')
+
+import time
+print("ведите start что бы запустить")
+input("")
+print('loading.', end="")
+print('\r', end='')
+time.sleep(1)
+print('loading..', end="")
+print('\r', end='')
+time.sleep(0.50)
+print('loading...', end="")
+print('\r', end='')
+time.sleep(0.50)
+print('loading:  :', end="")
+print('\r', end='')
+time.sleep(0.50)
+print('loading/ /', end="")
+print('\r', end='')
+time.sleep(0.50)
+print('loading\/:', end="")
+print('\r', end='')
+time.sleep(0.50)
+print('loading ', end="")
+print('\r', end='')
+print("войти?  ")
+input("")
+
+import tkinter
+
+root = tkinter.Tk()
+root.title("key.j app")
+root.geometry('500x500')
+
+label = Label(root, text = "hello user", width = 100, bg = 'black', fg = 'yellow')
+label.place(x = 60, y = 100)
+
+label.pack()
+
+bA = Button(root, text = "im keyj", width = 20, command = bA_click)
+bA.place(x = 130, y = 120)
+
+bB = Button(root, text = "im guest", width = 20, command = bB_click)
+bB.place(x = 280, y = 120)
+
+c = 1
+a = int(input())
+b = int(5466)
+if a > b:
+   print("доступ  разрешон")
+   print("введите имя")
+   name = input("")
+   print("what are you nead",name)
+   print("google:"), input("")
+   print("one moment")
+   print('loading.', end="")
+   print('\r', end='')
+   time.sleep(1)
+   print('loading..', end="")
+   print('\r', end='')
+   time.sleep(0.50)
+   print('loading...', end="")
+   print('\r', end='')
+   time.sleep(0.50)
+   print('loading:  :', end="")
+   print('\r', end='')
+   time.sleep(0.50)
+   print('loading/ /', end="")
+   print('\r', end='')
+   time.sleep(0.50)
+   print('loading\/:', end="")
+   print('\r', end='')
+   time.sleep(0.50)
+   print('loading ', end="")
+   print('\r', end='')
+   print("eror &^&^&^&^&^&")
+   print('\\;\\/:', end="")
+   print('\r', end='')
+   time.sleep(0.50)
+   print('#$@*% ', end="")
+   print('\r', end='')
+else:
+     print("access denied")
+
+
+root = mainloop()
+master.mainloop()
+time.sleep(5)
+master.destroy()
+time.sleep(5)
+mainloop.destroy()
+
+
+
+ 
+
+
+
+
+
